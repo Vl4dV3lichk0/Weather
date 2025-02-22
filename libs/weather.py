0b2548e2459abbbf35f80b25870451098f16daa3
Vlad <vladvelichko04@mail.ru>
2025-02-22 20:05:36 +0300

    removed extras

--- libs/weather.py
+++ libs/weather.py
@@ -1,5 +1,3 @@
-from os import strerror
-
 from googletrans import  Translator
 from autocorrect import Speller
 import requests
@@ -64,12 +62,9 @@ class Weather:
                                params={'q': self.s_city, 'type': 'like', 'units': 'metric', 'APPID': self.appid})
             self.data = self.res.json()
             self.cities = ["{} ({})".format(d['name'], d['sys']['country']) for d in self.data['list']]
-            #print("city: ", cities)
             self.city_id = self.data['list'][0]['id']
-            #print("city_id: ", city_id)
             logging.info("Initialization is successful")
         except Exception as e:
-            #print("Exception (init): ", e)
             logging.error(f"Exception (init): {e}")
             pass
         finally:
@@ -81,7 +76,6 @@ class Weather:
             self.res = requests.get("http://api.openweathermap.org/data/2.5/weather",
                                     params={'id': self.city_id, 'units': 'metric', 'lang': 'ru', 'APPID': self.appid})
             self.data = self.res.json()
-            #print(self.data)
 
             self.summary = self.get_coords, self.get_weather, self.get_main, self.get_visibility, self.get_wind, self.get_sun
             for function in self.summary:
@@ -89,7 +83,6 @@ class Weather:
 
             logging.info("Gathering is successful")
         except Exception as e:
-            #print("Exception (gather): ", e)
             logging.warning(f"Exception (get): {e}")
             pass
 
@@ -98,13 +91,11 @@ class Weather:
         try:
             self.lon = self.data['coord']['lon']
         except Exception as e:
-            # print("Exception (get_coords): ", e)
             logging.warning(f"Exception (get_coords_lon): {e}")
             pass
         try:
             self.lon = self.data['coord']['lat']
         except Exception as e:
-            # print("Exception (get_coords): ", e)
             logging.warning(f"Exception (get_coords_lat): {e}")
             pass
 
@@ -113,13 +104,11 @@ class Weather:
         try:
             self.conditions = self.data['weather'][0]['main']
         except Exception as e:
-            # print("Exception (get_weather_main): ", e)
             logging.warning(f"Exception (get_weather_main): {e}")
             pass
         try:
             self.description = self.data['weather'][0]['description']
         except Exception as e:
-            # print("Exception (get_weather_description): ", e)
             logging.warning(f"Exception (get_weather_description): {e}")
             pass
 
@@ -128,49 +117,41 @@ class Weather:
         try:
             self.temp = self.data['main']['temp']
         except Exception as e:
-            # print("Exception (get_main_temp): ", e)
             logging.warning(f"Exception (get_main_temp): {e}")
             pass
         try:
             self.feels_like = self.data['main']['feels_like']
         except Exception as e:
-            # print("Exception (get_main_feels_like): ", e)
             logging.warning(f"Exception (get_main_feels_like): {e}")
             pass
         try:
             self.temp_min = self.data['main']['temp_min']
         except Exception as e:
-            # print("Exception (get_main_temp_min): ", e)
             logging.warning(f"Exception (get_main_temp_min): {e}")
             pass
         try:
             self.temp_max = self.data['main']['temp_max']
         except Exception as e:
-            # print("Exception (get_main_temp_max): ", e)
             logging.warning(f"Exception (get_main_temp_max): {e}")
             pass
         try:
             self.pressure = self.data['main']['pressure']
         except Exception as e:
-            # print("Exception (get_main_pressure): ", e)
             logging.warning(f"Exception (get_main_pressure): {e}")
             pass
         try:
             self.humidity = self.data['main']['humidity']
         except Exception as e:
-            # print("Exception (get_main_humidity): ", e)
             logging.warning(f"Exception (get_main_humidity): {e}")
             pass
         try:
             self.sea_level = self.data['main']['sea_level']
         except Exception as e:
-            # print("Exception (get_main_sea_level): ", e)
             logging.warning(f"Exception (get_main_sea_level): {e}")
             pass
         try:
             self.grnd_level = self.data['main']['grnd_level']
         except Exception as e:
-            # print("Exception (get_main_grnd_level): ", e)
             logging.warning(f"Exception (get_main_grnd_level): {e}")
             pass
 
@@ -179,7 +160,6 @@ class Weather:
         try:
             self.grnd_level = self.data['visibility']
         except Exception as e:
-            # print("Exception (get_visibility): ", e)
             logging.warning(f"Exception (get_visibility): {e}")
             pass
 
@@ -188,19 +168,16 @@ class Weather:
         try:
             self.wind_speed = self.data['wind']['speed']
         except Exception as e:
-            # print("Exception (get_wind_speed): ", e)
             logging.warning(f"Exception (get_wind_speed): {e}")
             pass
         try:
             self.wind_deg = self.data['wind']['deg']
         except Exception as e:
-            # print("Exception (get_wind_deg): ", e)
             logging.warning(f"Exception (get_wind_deg): {e}")
             pass
         try:
             self.wind_gust = self.data['wind']['gust']
         except Exception as e:
-            # print("Exception (get_wind_gust): ", e)
             logging.warning(f"Exception (get_wind_gust): {e}")
             pass
 
@@ -209,13 +186,11 @@ class Weather:
         try:
             self.sunrise = self.data['sys']['sunrise']
         except Exception as e:
-            # print("Exception (get_sys_sunrise): ", e)
             logging.warning(f"Exception (get_sys_sunrise): {e}")
             pass
         try:
             self.sunset = self.data['sys']['sunset']
         except Exception as e:
-            # print("Exception (get_sys_sunset): ", e)
             logging.warning(f"Exception (get_sys_sunset): {e}")
             pass
 
@@ -228,6 +203,5 @@ class Weather:
             print("Ветер (напр. в град.):", int(self.wind_deg))
             logging.info("Showing is successful")
         except Exception as e:
-            #print("Exception (show_short):", e)
             logging.error(f"Exception (show_short): {e}")
             pass
